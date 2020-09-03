1a9fac1da7bf53ba5c9fa513b910a60de111287b
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-09-02 23:49:54 -0300

    Add files via upload

---
+++ Classe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ComparaIdadePessoas   //Precisa ter o mesmo nome do programa principal e estar no mesmo diretório
+{
+    public class Pessoa //Declaração da Classe Pessoa e suas características.
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ComparaIdadePessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1, p2;  //Declaração das variáveis.
+            
+            p1 =new Pessoa();   //Atribuição de classe.
+            p2 =new Pessoa();
+            
+            Console.Write("Informe o nome da primeira pessoa: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Informe a idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+            Console.Write("Informe o nome da segunda pessoa: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Informe a idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("A pessoa mais velha é " + p1.Nome);
+            }
+            else if (p1.Idade<p2.Idade)
+            {
+                Console.WriteLine("A pessoa mais velha é " + p2.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Ambas têm a mesma idade");
+            }
+            Console.ReadKey();
+        }
+    }
+}
